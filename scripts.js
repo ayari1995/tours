fa6714db4e597612539a909d4216df7bdbe3849c
ayari1995 <ayariachref1995@gmail.com>
2020-02-16 19:16:32 +0100

    first committ

---
+++ scripts.js
@@ -0,0 +1,10 @@
+//start
+const selectElement = s => document.querySelector(s);
+//open menu
+selectElement('.open').addEventListener('click', () => {
+  selectElement('.nav-list').classList.add('active');
+});
+//close menu
+selectElement('.close').addEventListener('click', () => {
+  selectElement('.nav-list').classList.remove('active');
+});
